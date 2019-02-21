bbf5c07c71215eb3af0d1df39d7188611c085481
James Woo <james.e.woo@gmail.com>
2019-02-21 14:03:52 -0500

    Refactor deploy script

--- scripts/mms/deploy.sh
+++ scripts/mms/deploy.sh
@@ -1,31 +1,57 @@
 #!/usr/bin/env bash
 
-lang=zh
+# custom handler properties
+
+# language to translate
+lang="zh"
+# Python package.module:function
+handler="services.zh_handler:handle"
+
+# model-archiver properties
+
+model_name="${lang}"
+# where the MAR will be saved
+export_path="/tmp/models"
+# where the model files live
+model_path="${export_path}/${model_name}"
+
+# Docker properties
+
+# predictions API port
+pred_port=8080
+# management API port
+mgmt_port=8081
+pred_url="http://localhost:${pred_port}"
+mgmt_url="http://localhost:${mgmt_port}"
+
+model_url="${model_name}"
+#model_url="${model_name}.mar"
+#model_url="https://www.dropbox.com/s/pk7hmp7a5zjcfcj/zh.mar?dl=1"
 
 if [[ "$1" = "update" ]]; then
-    scripts_dir=/tmp/models/${lang}/scripts
-    mkdir -p ${scripts_dir}
-    cp -r scripts/joshua/* ${scripts_dir}
-    cp -r scripts/moses/* ${scripts_dir}
-    cp -r src/services /tmp/models/${lang}
-    model-archiver -f --runtime python3 --export-path /tmp/models/ \
-        --model-name ${lang} --model-path /tmp/models/${lang} --handler services.${lang}_handler:handle
+    # update the scripts and handler files
+    mkdir -p ${model_path}/scripts
+    cp -r scripts/joshua/* ${model_path}/scripts
+    cp -r scripts/moses/* ${model_path}/scripts
+    cp -r src/services ${model_path}
+    model-archiver -f --runtime python3 --export-path ${export_path} \
+        --model-name ${model_name} --model-path ${model_path} --handler ${handler}
 fi
 
+# restart the Docker container
 docker kill mms
 docker rm mms
-docker run -itd --name mms -p 8080:8080 -p 8081:8081 -v /tmp/models:/opt/ml/model jwoo11/sockeye-serving
+docker run -itd --name mms -p ${pred_port}:8080 -p ${mgmt_port}:8081 \
+    -v ${export_path}:/opt/ml/model jwoo11/sockeye-serving
 
-#URL="${lang}.mar"
-URL="${lang}"
-#URL="https://www.dropbox.com/s/pk7hmp7a5zjcfcj/zh.mar?dl=1"
-
-until curl -X POST "http://localhost:8081/models?synchronous=true&initial_workers=1&url=${URL}"
+# load a model
+until curl -X POST "${mgmt_url}/models?synchronous=true&initial_workers=1&url=${model_url}"
 do
   echo "Waiting for initialization..."
   sleep 1
 done
 
-curl -X GET "http://localhost:8081/models/${lang}"
-curl -X POST "http://localhost:8080/predictions/${lang}" -H "Content-Type: application/json" \
+# test prediction
+curl -X GET "${mgmt_url}/models/${model_name}"
+curl -X POST "${pred_url}/predictions/${model_name}" -H "Content-Type: application/json" \
     -d '{ "text": "我的世界是一款開放世界遊戲，玩家沒有具體要完成的目標，即玩家有超高的自由度選擇如何玩遊戲" }' &
