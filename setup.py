940822c7f7d9fb7871fe4aa1c7986596007687d0
James Woo <james.e.woo@gmail.com>
2019-04-19 14:52:15 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,10 @@ from setuptools import setup, find_packages
 setup(
     name='sockeye-serving',
     version='1.0.0',
+    install_requires=['mxnet-model-server', 'pyyaml', 'requests'],
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    package_data={'sockeye_serving': ['scripts/*']},
+    package_data={'sockeye_serving': ['scripts/*', 'scripts/nonbreaking_prefixes/*']},
     scripts=glob(os.path.join('bin', '*')),
     data_files=[('config', glob(os.path.join('config', '*'))),
                 ('notebooks', glob(os.path.join('notebooks', '*')))],
