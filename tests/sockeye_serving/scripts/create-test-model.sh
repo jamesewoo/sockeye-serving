45ddce47ce5b5e3fb17c3205a51f0afd4381fd58
James Woo <james.e.woo@gmail.com>
2019-05-02 17:27:05 -0400

    Simplify test model

--- tests/sockeye_serving/scripts/create-test-model.sh
+++ tests/sockeye_serving/scripts/create-test-model.sh
@@ -11,26 +11,11 @@ python -m sockeye.train -s train.de \
                         -o tiny \
                         --encoder=rnn \
                         --decoder=rnn \
+                        --num-layers=1:1 \
                         --rnn-num-hidden=2 \
-                        --rnn-attention-in-upper-layers \
-                        --rnn-attention-type=dot \
-                        --rnn-decoder-hidden-dropout=0.2 \
-                        --embed-dropout=0.2 \
-                        --num-layers=8:8 \
-                        --weight-init=xavier \
-                        --weight-init-scale=3.0 \
-                        --weight-init-xavier-factor-type=avg \
                         --num-embed=1:1 \
-                        --max-seq-len=100 \
-                        --optimizer=adam \
-                        --optimized-metric=perplexity \
-                        --initial-learning-rate=0.0001 \
-                        --learning-rate-reduce-num-not-improved=8 \
-                        --learning-rate-reduce-factor=0.7 \
-                        --max-num-checkpoint-not-improved=1 \
+                        --num-words=1:1 \
                         --batch-type=sentence \
-                        --batch-size=128 \
-                        --checkpoint-frequency=2000 \
-                        --decode-and-evaluate=500 \
-                        --keep-last-params=60
-                        
+                        --batch-size=1 \
+                        --max-updates=4 \
+                        --checkpoint-frequency=2
